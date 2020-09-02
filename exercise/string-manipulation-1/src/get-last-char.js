7f3bcc17d46ba8b826c92d8257824f80e8dddb11
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-09-02 00:16:45 +0000

    Moved Files into exercise

---
+++ exercise/string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+
+function getLastChar(string){ 
+    return string.charAt(string.length - 1)
+
+}
---
+++ exercise/string-manipulation-1/src/is-upper-cased.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+function isUpperCased (word){ 
+    return word === word.toUpperCase();
+} 
+// define a function 
+// condition if all characters in the word are uppercased
+// if so return true
+// otherwise false 

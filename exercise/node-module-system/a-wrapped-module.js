1029c974a24a2fe1cd62b15f3451532d55ed89b5
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-09-02 23:29:58 +0000

    Finished node-module-system

---
+++ exercise/node-module-system/a-wrapped-module.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+console.log("exports", typeof exports)
+console.log("exports",  exports)
+console.log("require", typeof require)
+console.log("require",  require)
+console.log("module", typeof module)
+console.log("module",  module)
+console.log("__filename", typeof __filename)
+console.log("__filename",  __filename)
+console.log("__dirname", typeof __dirname)
+console.log("__dirname", __dirname)

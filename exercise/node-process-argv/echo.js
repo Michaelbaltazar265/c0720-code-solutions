186c292131440989214961227b8bc7c6ee9882cb
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-09-04 04:41:09 +0000

    Fixed echo.js

--- exercise/node-process-argv/echo.js
+++ exercise/node-process-argv/echo.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
 // this is a message to see what I can see 
-console.log("I can see this messages clear as day!!!!")
-"Hello Michael"
+
+console.log(process.argv[2]);
